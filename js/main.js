f506fa89368629c9d0bd561621d188b0a462cb7f
AK Milad <miladahmed1099@gmail.com>
2022-06-13 18:44:19 +0600

    SMS Module Complete

--- js/main.js
+++ js/main.js
@@ -136,3 +136,45 @@ selectBtn.addEventListener("click", () => wrapper.classList.toggle("active"));
 // SMS Subject Title(Dropdonw With Text Search)
 
 
+
+
+
+
+// Time & Date Live 
+const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+var today = new Date();  /* new date object */
+var date = today.getDate() + ' - ' + month[(today.getMonth())] + ' - ' + today.getFullYear();
+/* display current date */
+document.getElementById("currentDate").innerHTML = date;
+
+/* Auto refreshing clock time */
+function startTime() {
+    var today = new Date(); /* new date object */
+    /* getting minutes hours and seconds from date object */
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
+    /* 12 hour time formate */
+    var amPm = "AM";
+    if (hours > 13) {
+        amPm = "PM";
+        hours = hours - 12;
+    }
+    /* put zero before numbers < 10 */
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    var time = hours + ' : ' + minutes + ' : ' + seconds + '  ' + amPm;
+    /* display current time */
+    document.getElementById("currentTime").innerHTML = time;
+
+    /* Auto refreshing time every 1 second */
+    setTimeout(function () { startTime() }, 1000);
+}
+// Time & Date Live 
